f497a593e14c27e68afb90129557cd3ce9ad697c
Tejendra <tejendrapalsingh7@gmail.com>
2020-08-05 19:18:29 +0530

    adsa

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(
 );
 
 app.use(express.static(path.join(rootDir, "public")));
-app.use("/image", express.static(path.join(".", "image")));
+app.use("/images", express.static(path.join(".", "image")));
 app.use(
     session({
         secret: "hello everyone what the duck is this",
